6ec1bb43973335d4cb1461a0bb3f2a7fe3772e66
Carter Buce <cmb9400@rit.edu>
2017-10-16 21:40:48 -0700

    generated spring application

---
+++ src/main/java/com/github/cmb9400/skipassistant/SkipAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.github.cmb9400.skipassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipAssistantApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/cmb9400/skipassistant/SkipAssistantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.cmb9400.skipassistant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkipAssistantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
