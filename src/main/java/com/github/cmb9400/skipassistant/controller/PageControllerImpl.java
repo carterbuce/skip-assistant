d2603fbad9c6559c08ac34d060ebda1103943a58
Carter Buce <cmb9400@rit.edu>
2017-10-17 20:29:21 -0700

    use slf4j logger instead of log4j

--- src/main/java/com/github/cmb9400/skipassistant/controller/PageControllerImpl.java
+++ src/main/java/com/github/cmb9400/skipassistant/controller/PageControllerImpl.java
@@ -1,14 +1,14 @@
 package com.github.cmb9400.skipassistant.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageControllerImpl implements PageController {
-    private static final Logger LOGGER = LogManager.getLogger(PageControllerImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PageControllerImpl.class);
 
     @Override
     public String index(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
